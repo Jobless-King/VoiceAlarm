e7a6ca057928aaae44638ec9770b50c713f162cc
YeomChealMin <ymc12377@naver.com>
2016-09-18 19:55:58 +0900

    DB add
    DB add

---
+++ app/src/main/java/com/galaxy/voicealarm/DBHelper.java
@@ -0,0 +1,25 @@
+package com.galaxy.voicealarm;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+public class DBHelper extends SQLiteOpenHelper{
+    public DBHelper(Context context, String DBName){
+        super(context, DBName, null, 1);
+    }
+    public void onCreate(SQLiteDatabase db){
+        db.execSQL("CREATE TABLE Schedule(_id integer primary key autoincrement, datetime double, content text)");
+        db.execSQL("CREATE TABLE Alarm(_id integer primary key autoincrement, week integer, name text)");
+    }
+    public void onUpgrade(SQLiteDatabase db, int oloVersion, int newVersion){
+        db.execSQL("DROP TABLE IF EXISTS Schedule");
+        db.execSQL("DROP TABLE IF EXISTS Alarm");
+        onCreate(db);
+    }
+    public void query(String query) {
+        SQLiteDatabase db = getWritableDatabase();
+        db.execSQL(query);
+    }
+}
--- app/src/main/java/com/galaxy/voicealarm/MainActivity.java
+++ app/src/main/java/com/galaxy/voicealarm/MainActivity.java
@@ -7,13 +7,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.naver.naverspeech.kfgd_naver.IManagerCommand;
+import com.naver.naverspeech.kfgd_naver.NaverSpeechManager;
 
+public class MainActivity extends AppCompatActivity {
+    NaverSpeechManager naverSpeechManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        naverSpeechManager = NaverSpeechManager.CreateNaverSpeechManager(this, "", new VoiceManager());
     }
+
+
 
     public void RunAddAlarm(View view){
         Intent intent=new Intent(MainActivity.this, AddAlarm.class);
