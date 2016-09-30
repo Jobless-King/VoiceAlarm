3e6093e4257fdc296845a34a061521d7864986b6
GwanYong Kim <kky960105@naver.com>
2016-09-30 15:05:29 +0900

    modify layout(editMemoDialog, main)

--- app/src/main/java/com/galaxy/voicealarm/MainActivity.java
+++ app/src/main/java/com/galaxy/voicealarm/MainActivity.java
@@ -65,6 +65,7 @@ public class MainActivity extends AppCompatActivity implements IRefresh{
         //Set Decorators
         calendarView.setDecorators(decorators);
         calendarView.refreshCalendar(currentCalendar);
+        resetSelectedMemo();
     }
 
     @Override
@@ -76,8 +77,7 @@ public class MainActivity extends AppCompatActivity implements IRefresh{
     @Override
     public void Refresh() {
         refreshMemoList();
-        selectedMemo = null;
-        memoText.setText("");
+        resetSelectedMemo();
     }
 
     public void RunAlarmList(View view){
@@ -106,6 +106,7 @@ public class MainActivity extends AppCompatActivity implements IRefresh{
                 //SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy");
                 SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
                 //Toast.makeText(MainActivity.this, df.format(date), Toast.LENGTH_SHORT).show();
+                memoBtn.setVisibility(View.VISIBLE);
                 selectedMemo = memoList.get(df.format(date));
                 if (null != selectedMemo)
                     memoText.setText(selectedMemo.getContent());
@@ -119,8 +120,15 @@ public class MainActivity extends AppCompatActivity implements IRefresh{
             public void onMonthChanged(Date date) {
                 SimpleDateFormat df = new SimpleDateFormat("MM-yyyy");
                 //Toast.makeText(MainActivity.this, df.format(date), Toast.LENGTH_SHORT).show();
+                resetSelectedMemo();
             }
         });
+    }
+
+    private void resetSelectedMemo(){
+        selectedMemo = null;
+        memoText.setText("");
+        memoBtn.setVisibility(View.GONE);
     }
 
     private void refreshMemoList(){
