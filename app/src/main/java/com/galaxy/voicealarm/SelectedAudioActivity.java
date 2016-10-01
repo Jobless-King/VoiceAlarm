6b540fc77020a214d2df1301cb03552d622d9493
Kim Gwan Yong <kky960105@naver.com>
2016-10-01 23:09:02 +0900

    AddAlarm_Select Audio

---
+++ app/src/main/java/com/galaxy/voicealarm/SelectedAudioActivity.java
@@ -0,0 +1,136 @@
+package com.galaxy.voicealarm;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class SelectedAudioActivity extends AppCompatActivity {
+
+    String mCurrent;
+    String mRoot;
+    TextView mCurrentTxt;
+    ListView mFileList;
+    ArrayAdapter<String> mAdapter;
+    ArrayList<String> arFiles;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selected_audio);
+
+        mCurrentTxt = (TextView)findViewById(R.id.current);
+        mFileList = (ListView)findViewById(R.id.filelist);
+
+        arFiles = new ArrayList<String>();
+        mRoot = Environment.getExternalStorageDirectory().getAbsolutePath();
+        mCurrent = mRoot;
+
+        mAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, arFiles);
+        mFileList.setAdapter(mAdapter);
+        mFileList.setOnItemClickListener(mItemClickListener);
+
+        refreshFiles();
+    }
+
+    //Root Page일 경우에는 Activity나가기
+    //아닐 경우에는 이전 디렉토리로 이동
+    @Override
+    public void onBackPressed(){
+        if( 0 == mRoot.compareTo(mCurrent)){
+            finish();
+        }else{
+            int end = mCurrent.lastIndexOf("/");
+            String uppath = mCurrent.substring(0, end);
+            mCurrent = uppath;
+            refreshFiles();
+        }
+    }
+
+    AdapterView.OnItemClickListener mItemClickListener =
+            new AdapterView.OnItemClickListener(){
+
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long ld) {
+                    String Name = arFiles.get(position);
+                    if(Name.startsWith("[")&&Name.endsWith("]")){
+                        Name = Name.substring(1, Name.length()-1);
+                    }
+                    String Path = mCurrent + "/" + Name;
+                    File f = new File(Path);
+                    if(f.isDirectory()){
+                        mCurrent = Path;
+                        refreshFiles();
+                    }
+                    //디렉토리가 아닌 파일인 경우
+                    else{
+                        String fileName = arFiles.get(position);
+                        Toast.makeText(SelectedAudioActivity.this, Uri.fromFile(f).toString(),
+                                Toast.LENGTH_SHORT).show();
+                        //mp3파일인 경우, 음악 실행시킴
+                        if(0 == ".mp3".compareTo(fileName.substring(fileName.length()-4, fileName.length()))){
+                            Intent intent = new Intent();
+                            AudioFile audioFile = new AudioFile(fileName, Uri.fromFile(f).toString());
+                            intent.putExtra("AUDIO_FILE", audioFile);
+                            setResult(RESULT_OK, intent);
+                            finish();
+                        }else{
+                            Toast.makeText(SelectedAudioActivity.this, "mp3파일을 선택해주세요", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+            };
+
+    public void mOnClick(View v){
+        switch (v.getId()){
+            case R.id.btnroot:
+                if(0 != mCurrent.compareTo(mRoot)){
+                    mCurrent = mRoot;
+                    refreshFiles();
+                }
+                break;
+            case R.id.btnup:
+                if(0 != mCurrent.compareTo(mRoot)){
+                    int end = mCurrent.lastIndexOf("/");
+                    String uppath = mCurrent.substring(0, end);
+                    mCurrent = uppath;
+                    refreshFiles();
+                }
+                break;
+        }
+    }
+
+    //디렉토리에 있는 file들로 arFiles를 채우는 코드
+    void refreshFiles(){
+        mCurrentTxt.setText(mCurrent);
+        arFiles.clear();
+        File current = new File(mCurrent);
+        String[] files = current.list();
+        if(null != files){
+            for(int i=0; i<files.length; ++i){
+                String Path = mCurrent + "/" + files[i];
+                String Name = "";
+                File f = new File(Path);
+                if(f.isDirectory()){
+                    Name = "[" + files[i] + "]";
+                }else{
+                    Name = files[i];
+                }
+
+                arFiles.add(Name);
+            }
+        }
+        mAdapter.notifyDataSetChanged();
+    }
+}
