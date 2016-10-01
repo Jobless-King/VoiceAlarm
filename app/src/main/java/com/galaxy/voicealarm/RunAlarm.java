47b90a422954cde5a5f884079b56aeb0b5ae55c4
YeomChealMin <ymc12377@naver.com>
2016-10-01 11:11:33 +0900

    Alarm remove logicaly complete and check string
    Alarm remove logicaly complete and check string

--- app/src/main/java/com/galaxy/voicealarm/RunAlarm.java
+++ app/src/main/java/com/galaxy/voicealarm/RunAlarm.java
@@ -1,5 +1,7 @@
 package com.galaxy.voicealarm;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
@@ -10,6 +12,7 @@ import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,6 +21,7 @@ import com.naver.naverspeech.kfgd_naver.NaverSpeechManager;
 
 import org.w3c.dom.Text;
 
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -27,11 +31,12 @@ public class RunAlarm extends AppCompatActivity implements IManagerCommand {
     NaverSpeechManager naverSpeechManager;
     private MediaPlayer mediaPlayer;
     private Vibrator vibrator;
-    private String[] voice, madeString;
+    private String voice, madeString;
     private TextView command, read, txt1, txt2;
     private DBHelper dbHelper;
     private SQLiteDatabase sql;
     private Cursor cursor;
+    private int curTime;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,12 +54,14 @@ public class RunAlarm extends AppCompatActivity implements IManagerCommand {
 
         cursor = CurrentAlarmExist(cursor);
         if (cursor != null){
+            RegisterNextAlarm(cursor);
             if(CurrentAlarmIsOn(cursor))
                 RunCurrentAlarm();
             else
                 this.finish();
         }else
             this.finish();
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
     }
     @Override
     public void clientReady() {
@@ -69,7 +76,7 @@ public class RunAlarm extends AppCompatActivity implements IManagerCommand {
     @Override
     public void finalResult(String[] finalText) {
         txt2.setText(finalText[0]);
-        voice = finalText;
+        voice = finalText[0];
     }
     @Override
     public void recognitionError(String errorText) {
@@ -77,9 +84,7 @@ public class RunAlarm extends AppCompatActivity implements IManagerCommand {
     }
     @Override
     public void clientInactive() {
-        if(voice.equals(madeString))
-            Kill(null);
-        txt1.setText("Connect end");
+        Check();
     }
     public void MicOn(View view){
         if (!naverSpeechManager.getRecognizeState()) {
@@ -120,7 +125,7 @@ public class RunAlarm extends AppCompatActivity implements IManagerCommand {
     private Cursor CurrentAlarmExist(Cursor cursor){
         boolean isRun = false;
         SimpleDateFormat df = new SimpleDateFormat("HHmm", Locale.KOREA);
-        int curTime = Integer.parseInt(df.format(new Date()));
+        curTime = Integer.parseInt(df.format(new Date()));
 
         cursor.moveToFirst();
         if(cursor.getCount() > 0) {
@@ -131,6 +136,17 @@ public class RunAlarm extends AppCompatActivity implements IManagerCommand {
             }
         }
         return null;
+    }
+    private void RegisterNextAlarm(Cursor cursor){
+        int selectedHour, selectedMinute;
+        selectedHour = curTime/100;
+        selectedMinute = curTime%100;
+        AlarmManager alarmManager = (AlarmManager)getApplicationContext().getSystemService(Context.ALARM_SERVICE);
+        Intent Intent = new Intent(this, RunAlarm.class);
+        PendingIntent pIntent = PendingIntent.getActivity(getApplicationContext(), 0, Intent, 0);
+        long settingTime = System.currentTimeMillis() - ((System.currentTimeMillis()+9*60*60*1000)%(24*60*60*1000)) + selectedHour*60*60*1000 + selectedMinute*60*1000;
+        //alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, settingTime, 24*60*60*1000, pIntent);
+        alarmManager.set(AlarmManager.RTC_WAKEUP,settingTime+(24*60*60*1000), pIntent);
     }
     private boolean CurrentAlarmIsOn(Cursor cursor){
         if(1!=cursor.getInt(cursor.getColumnIndex("alive")))
@@ -173,13 +189,13 @@ public class RunAlarm extends AppCompatActivity implements IManagerCommand {
 
         if (memo != null) {
             //일정이 있을때
-            madeString = memo.toString().split(" ");
+            madeString = memo.getContent();
             command.setText("오늘 할일은?");
             read.setText(memo.getContent());
         } else {
-            madeString = new String[]{"일찍", "일어난", "벌래"};
+            madeString = ("일찍 일어난 벌레");
             command.setText("할일은 없지만 일어나렴");
-            read.setText("일찍 일어난 벌래");
+            read.setText("일찍 일어난 벌레");
         }
         mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.escape);
         mediaPlayer.setLooping(true);
@@ -187,5 +203,11 @@ public class RunAlarm extends AppCompatActivity implements IManagerCommand {
         long[] pattern = { 0, 500, 200, 400, 100 };
         vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
         vibrator.vibrate(pattern, 2);
+    }
+    private void Check(){
+        txt1.setText("Connect end");
+        if(voice.equals(madeString)) {
+            Kill(null);
+        }
     }
 }
